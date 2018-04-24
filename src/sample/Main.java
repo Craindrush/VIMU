244599767b61ca3f6052c56a8887c80b6840d07f
Omar Osuna <omar.alonso.osuna.angulo@uabc.edu.mx>
2018-04-24 11:02:19 -0700

    Proyecto creado. Layout del menu elaborado e inclusion de assets

---
+++ src/sample/Main.java
@@ -0,0 +1,26 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("view/menu.fxml"));
+        primaryStage.setResizable(false);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+
+        primaryStage.setScene(new Scene(root, 1280, 768));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

be8115a30806d022aa332e1220bdd2b921c87ab3
OMAR OSUNA <omarosuna98@gmail.com>
2018-04-25 01:15:29 -0700

    Adición de los layouts de registro, spanishquiz y mejora de ranking, junto con sus respectivos asses

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/menu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/ranking.fxml"));
         primaryStage.setResizable(false);
         primaryStage.initStyle(StageStyle.UNDECORATED);
 
